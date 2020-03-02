746fccc2b35b7f855e47204739936d283b3fb69c
Nadeesha Dilruwan <eclayaz@gmail.com>
2020-03-02 19:03:41 +0530

    add reducer

--- src/App.js
+++ src/App.js
@@ -2,12 +2,10 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import { Provider } from "react-redux";
-import { createStore, applyMiddleware } from "redux";
 
 import Posts from "./components/Posts";
 import PostForm from "./components/PostForm";
-
-const store = createStore(() => [], {}, applyMiddleware());
+import store from "./store";
 
 function App() {
   return (

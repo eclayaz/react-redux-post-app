17109d70c7f17b87c3bbd8632746767b09c0ea63
Nadeesha Dilruwan <eclayaz@gmail.com>
2020-03-02 18:50:45 +0530

    use provider

--- src/App.js
+++ src/App.js
@@ -1,19 +1,26 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+
 import Posts from "./components/Posts";
 import PostForm from "./components/PostForm";
 
+const store = createStore(() => [], {}, applyMiddleware());
+
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <PostForm />
-      <hr />
-      <Posts />
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+        </header>
+        <PostForm />
+        <hr />
+        <Posts />
+      </div>
+    </Provider>
   );
 }
 
